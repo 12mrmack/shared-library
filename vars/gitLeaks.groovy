b3063dc2511f1845930b2b0e7f3a66979d27f6fa
Maqbool Alam <50446979+12mrmack@users.noreply.github.com>
2026-05-17 16:50:59 +0530

    Update gitLeaks.groovy

--- vars/gitLeaks.groovy
+++ vars/gitLeaks.groovy
@@ -1,6 +1,5 @@
 def call(String report_file_name){
 	stage('GitLeaks Scan') {
-            steps {
                 sh '''
                 /usr/local/bin/gitleaks detect \
                 --source . \
@@ -8,11 +7,8 @@ def call(String report_file_name){
                 --report-path ${report_file_name} || true
                 '''
             }
-        }
         
         stage('Print GitLeaks Report') {
-            steps {
                 sh 'cat ${report_file_name} || true'
-            }
         }
 }
