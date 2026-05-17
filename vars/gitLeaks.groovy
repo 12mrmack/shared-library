26793eea9cf9b87ded92cc82923548e4d6c15b69
Maqbool Alam <50446979+12mrmack@users.noreply.github.com>
2026-05-17 16:26:48 +0530

    Add GitLeaks scanning functionality

---
+++ vars/gitLeaks.groovy
@@ -0,0 +1,18 @@
+def call(string report_file_name){
+	stage('GitLeaks Scan') {
+            steps {
+                sh '''
+                /usr/local/bin/gitleaks detect \
+                --source . \
+                --report-format json \
+                --report-path ${report_file_name} || true
+                '''
+            }
+        }
+        
+        stage('Print GitLeaks Report') {
+            steps {
+                sh 'cat ${report_file_name} || true'
+            }
+        }
+}
