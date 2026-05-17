2afce1fc6cc8dd2d85a3fc43c63d357ef1c146b8
Maqbool Alam <50446979+12mrmack@users.noreply.github.com>
2026-05-17 17:01:07 +0530

    Refactor GitLeaks function for improved readability

--- vars/gitLeaks.groovy
+++ vars/gitLeaks.groovy
@@ -1,14 +1,15 @@
-def call(String report_file_name){
-	stage('GitLeaks Scan') {
-                sh '''
-                /usr/local/bin/gitleaks detect \
-                --source . \
-                --report-format json \
-                --report-path '${report_file_name}' || true
-                '''
-            }
-        
-        stage('Print GitLeaks Report') {
-                sh 'cat ${report_file_name} || true'
-        }
+def call(String report_file_name) {
+
+    stage('GitLeaks Scan') {
+        sh """
+        /usr/local/bin/gitleaks detect \
+        --source . \
+        --report-format json \
+        --report-path ${report_file_name} || true
+        """
+    }
+
+    stage('Print GitLeaks Report') {
+        sh "cat ${report_file_name} || true"
+    }
 }
